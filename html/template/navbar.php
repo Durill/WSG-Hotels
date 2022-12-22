6df8c487b37eaf44b6e20e69e9a067de4d9e3432
Wojtek <wojbat4@gmail.com>
2022-12-22 22:46:32 +0100

    Change footer color

--- html/template/navbar.php
+++ html/template/navbar.php
@@ -11,10 +11,8 @@
       <meta name="author" content="">
       <link rel="stylesheet" href="css/style.css">
       <link rel="stylesheet" href="css/own-style.css">
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
    </head>
