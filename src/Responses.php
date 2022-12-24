b9827a44895d8c838b825225b05446cfb79635bb
Wojtek <wojbat4@gmail.com>
2022-12-24 04:42:21 +0100

    Fix warning alert view

--- src/Responses.php
+++ src/Responses.php
@@ -11,15 +11,15 @@ include_once __DIR__ . '/StatusesEnum.php';
                             Rejestracja powiodła się! Teraz możesz się zalogować.
                             </div>';
                 case StatusesEnum::FAILED:
-                    return  nl2br('<div class="alert alert-warning alert-dismissible">
+                    return  '<div class="alert alert-warning alert-dismissible">
                             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                             Podane dane są niepoprawne, sprawdź czy jest zgodne z naszymi zasadami:
-                              -Imię musi mieć minimum 3 znaki i maksimum 25
-                              -Nazwisko musi mieć minimum 3 znaki i maksimum 50
-                              -Hasła muszą być takie same
-                              -Hasło musi mieć minimum 8 znaków
-                              -Konto email nie może być już w użyciu
-                            </div>');
+                              <br>-Imię musi mieć minimum 3 znaki i maksimum 25
+                              <br>-Nazwisko musi mieć minimum 3 znaki i maksimum 50
+                              <br>-Hasła muszą być takie same
+                              <br>-Hasło musi mieć minimum 8 znaków
+                              <br>-Konto email nie może być już w użyciu
+                            </div>';
                 case StatusesEnum::ERROR:
                     return  '<div class="alert alert-danger alert-dismissible">
                             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
